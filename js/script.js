1c1a2d77a74856f98172f0c3c36dc5e14a62d5ee
peppe965 <giuseppe.falco000@gmail.com>
2020-09-24 20:56:05 +0200

    milestone 4 ok

--- js/script.js
+++ js/script.js
@@ -18,8 +18,13 @@
     //aggiungiamo la copertina del film o della serie al nostro elenco.
     //Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
     //Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
+//Milestone 4:
+    //Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp, creando un layout completo simil-Netflix:
+    //Un header che contiene logo e search bar
+    //Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio la poster_path con w342)
+    //Andando con il mouse sopra una card (on hover), appaiono le informazioni aggiuntive già prese nei punti precedenti più la overview
 
-$(document).ready(function() {
+    $(document).ready(function() {
     //parte ricerca e stampa in pagina al click sulll botton cerca
     $("#search-button").click(function(){
         var input = $("#search-input").val(); 
@@ -39,8 +44,9 @@ $(document).ready(function() {
     //////////////////////////temporaneo////////////////
     
    
-
-
+    $(".logo-header").click(function(){
+        startSearch(" ");
+      }); 
 
 
 
@@ -210,12 +216,18 @@ $(document).ready(function() {
 
         switch (option) {
             case "search-film":
+                $("#wrapper>h2:first-of-type").show();
+                $("#wrapper>h2:last-of-type").hide();
                 searchFilm(input);
                 break;
             case "search-show":
+                $("#wrapper>h2:last-of-type").show();
+                $("#wrapper>h2:first-of-type").hide();
                 searchShow(input);
                 break;
             case "search-global":
+                $("#wrapper>h2:first-of-type").show();
+                $("#wrapper>h2:last-of-type").show();
                 searchFilm(input);
                 searchShow(input);
                 break; 
